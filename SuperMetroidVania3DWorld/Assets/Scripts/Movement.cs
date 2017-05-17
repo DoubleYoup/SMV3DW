9be904f538aebacbad47209bf9e2824f1396dd2d
Trevor Rau <trevor@firemindgames.org>
2017-05-17 09:28:13 -0500

    setting up project for GIT

---
+++ SuperMetroidVania3DWorld/Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+
+    //This Is a test commit
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
