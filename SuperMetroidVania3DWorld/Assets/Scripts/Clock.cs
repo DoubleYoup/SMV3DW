12e761e3ff44d3a05847f28e2992e50291af613f
Trevor Rau <trevor@firemindgames.org>
2017-05-21 19:14:50 -0500

    fix 2

---
+++ SuperMetroidVania3DWorld/Assets/Scripts/Clock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour 
+    {
+
+    public Transform hours, minitues, seconds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
