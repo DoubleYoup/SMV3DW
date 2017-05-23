256aea75321708067a50e14adacc5b00dc02fe53
Trevor Rau <trevor@firemindgames.org>
2017-05-22 20:54:38 -0500

    attempt to fix crash

--- SuperMetroidVania3DWorld/Assets/Scripts/Clock.cs
+++ SuperMetroidVania3DWorld/Assets/Scripts/Clock.cs
@@ -1,19 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Clock : MonoBehaviour 
     {
 
     public Transform hours, minitues, seconds;
 
+    private const float 
+        hoursToDegrees = 360f/12f,
+        minitesToDegrees = 360f/60f,
+        secondsToDegrees = 360f/60f;
+
+
+    public bool analog;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        if (analog)
+        {
+            TimeSpan timeSpan = DateTime.Now.TimeOfDay;
+            hours.localRotation = Quaternion.Euler(0f,0f,(float)timeSpan.TotalHours * -hoursToDegrees);
+            minitues.localRotation = Quaternion.Euler(0f,0f,(float)timeSpan.TotalMinutes * -minitesToDegrees);
+            seconds.localRotation = Quaternion.Euler(0f,0f,(float)timeSpan.TotalSeconds * -secondsToDegrees);
+        }
+        else
+        {
+            DateTime time = DateTime.Now;
+            hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+            minitues.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minitesToDegrees);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        }
 	}
 }
