0bc6b9df58efa24ae27abccbd4433503aa21786d
Trevor Rau <trevor@firemindgames.org>
2017-06-11 15:48:28 -0500

    Nucleon test

---
+++ SuperMetroidVania3DWorld/Assets/Scripts/Nucleon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nucleon : MonoBehaviour {
+
+    public float attractionForce;
+
+    Rigidbody body;
+
+    void Awake ()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SuperMetroidVania3DWorld/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawn;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+
+    private float timeSinceLastSpawn;
+
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawn)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawn;
+            SpawnNucleon();
+        }
+    }
+
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
